bb18ea2b974993efd5b80ae143a4ebf0010fd76f
Sean Carson <intracarson@gmail.com>
2020-06-14 23:12:29 -0400

    test

---
+++ e383.py
@@ -0,0 +1,25 @@
+# Challenge
+def same_necklace(first, second):
+    for i in range (len(second)):
+        if first == second[i:] + second[:i]:
+            return True
+    return False
+
+# Optional Bonus 1
+def repeats(first):
+    counter = 0
+    if first == "":
+        counter += 1
+        return counter
+    for i in range (len(first)):
+        if first == first[i:] + first[:i]:
+            counter += 1
+    return counter
+
+# Optional Bonus 2
+def parse():
+    enable1_file = open("enable1.txt", "r")
+    
+
+    enable1_file.close()
+parse()
